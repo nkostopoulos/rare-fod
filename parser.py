3426bcac16af438b849c0ef50a503283eb95b106
nkostopoulos <nkostopoulos@netmode.ntua.gr>
2024-05-03 22:35:29 +0300

    added FoD container and adjusted network settings

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import subprocess
+from time import sleep
 
 def return_command_output(command):
     proc = subprocess.Popen(command, stdout = subprocess.PIPE, shell = True)
@@ -7,36 +8,38 @@ def return_command_output(command):
     return output
 
 if __name__ == "__main__":
-    command = "ls /var/cache/nfdump/"
-    files = return_command_output(command).decode('utf8')
-    files_list = files.split("\n")
+    while True:
+        sleep(45)
+        command = "ls /var/cache/nfdump/"
+        files = return_command_output(command).decode('utf8')
+        files_list = files.split("\n")
+    
+        max_timestamp = -1
+        for afile in files_list:
+            timestamp = afile.split(".")[1]
+            if str(timestamp) == "current":
+                continue
+            else:
+                timestamp = int(timestamp)
+                if max_timestamp < timestamp:
+                    max_timestamp = timestamp
+        afile = "nfcapd." + str(max_timestamp)
+    
+        command = "nfdump -r /var/cache/nfdump/" + str(afile) + " -s srcip -a -N"
+        nfdump_output = return_command_output(command).decode('utf8')
+        nfdump_split = nfdump_output.split("\n")
+    
+        output_temp = nfdump_split[2]
+        output_temp = " ".join(output_temp.split())
+        output_temp = output_temp.split(" ")
+        
+        src_ip = output_temp[4]
+        flows = output_temp[5].split("(")[0]
+        packets = output_temp[6].split("(")[0] 
+        number_bytes = output_temp[7].split("(")[0]
+    
+        print("src_ip: ", src_ip)
+        print("flows: ", flows)
+        print("packets: ", packets)
+        print("number_bytes: ", number_bytes)
 
-    max_timestamp = -1
-    for afile in files_list:
-        timestamp = afile.split(".")[1]
-        if str(timestamp) == "current":
-            continue
-        else:
-            timestamp = int(timestamp)
-            if max_timestamp < timestamp:
-                max_timestamp = timestamp
-    afile = "nfcapd." + str(max_timestamp)
-
-    command = "nfdump -r /var/cache/nfdump/" + str(afile) + " -s srcip -a"
-    print(command)
-    nfdump_output = return_command_output(command).decode('utf8')
-    nfdump_split = nfdump_output.split("\n")
-
-    output_temp = nfdump_split[2]
-    output_temp = " ".join(output_temp.split())
-    output_temp = output_temp.split(" ")
-
-    src_ip = output_temp[4]
-    flows = output_temp[5] + " " + output_temp[6]
-    packets = output_temp[7] + " " + output_temp[8] 
-    number_bytes = output_temp[9] + " " + output_temp[10]
-
-    print("src_ip: ", src_ip)
-    print("flows: ", flows)
-    print("packets: ", packets)
-    print("number_bytes: ", number_bytes)
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 
 host1 = "clab-fod-lab-host1"
 host2 = "clab-fod-lab-host2"
+fod   = "clab-fod-lab-fod"
 
 def execute_command(host, command):
     arg = "docker exec -it " + str(host) + " " + str(command)
@@ -19,51 +20,81 @@ def debug(command):
     return None
 
 if __name__ == "__main__":
+    # Apt update in host1, host2 and FoD containers
     command = "apt update"
     debug(command)
     execute_command(host1, command)
     execute_command(host2, command)
+    execute_command(fod, command)
 
+    # Installation of package "net-tools" for ifconfig
     command = "apt install -y net-tools"
     debug(command)
     execute_command(host1, command)
     execute_command(host2, command)
+    execute_command(fod, command)
 
+    # Installation of package "inetutils-ping" for ping
     command = "apt install -y inetutils-ping"
     debug(command)
     execute_command(host1, command)
     execute_command(host2, command)
+    execute_command(fod, command)
 
+    # Installation of package "iproute2" for static routes
+    command = "apt install -y iproute2"
+    debug(command)
+    execute_command(host1, command)
+    execute_command(host2, command)
+
+    # Ifconfig commands to configure non-management container interfaces
     command = "ifconfig eth1 10.0.1.2"
     debug(command)
     execute_command(host1, command)
     command = "ifconfig eth1 10.0.2.2"
     execute_command(host2, command)
+    command = "ifconfig eth1 10.0.3.2"
+    execute_command(fod, command)
 
+    # Configuration of static routes on hosts host1 and host2
+    command = "ip route add 10.0.2.0/24 via 10.0.1.1"
+    debug(command)
+    execute_command(host1, command)
+    command = "ip route add 10.0.1.0/24 via 10.0.2.1"
+    debug(command)
+    execute_command(host2, command)
+
+    # Install of package "hping3" in host1 in order to DDoS host2
     command = "apt install -y hping3"
     debug(command)
     execute_command(host1, command)
 
+    # Install of package "nfdump" in FoD (NetFlow collector)
     command = "apt install -y nfdump"
     debug(command)
-    execute_command(host2, command)
+    execute_command(fod, command)
 
-    command = "nfcapd -b 10.0.2.2 -p 9995 -l /var/cache/nfdump/"
+    # Run "nfcapd" command to collect NetFlow records
+    command = "nfcapd -b 10.0.3.2 -p 9995 -l /var/cache/nfdump/ -t 30s"
     debug(command)
-    execute_command_detached(host2, command)
+    execute_command_detached(fod, command)
 
-    command = "docker cp parser.py clab-fod-lab-host2:/root/parser.py"
+    # Copy the "parser.py" script that parsers nfdump output
+    command = "docker cp parser.py clab-fod-lab-fod:/root/parser.py"
     debug(command)
     os.system(command)
 
+    # Install Python 3 within the FoD container
     command = "apt install -y python3"
     debug(command)
-    execute_command(host2, command)
+    execute_command(fod, command)
 
-    command = "timeout 10m hping3 -S 10.0.1.1 --flood"
+    # DDoS host2 from host1 using "hping3"
+    command = "timeout 10m hping3 -S 10.0.2.2 --flood"
     debug(command)
     execute_command_detached(host1, command)
 
+    # Run the "parser.py" Python script
     command = "python3 /root/parser.py"
     debug(command)
-    execute_command(host2, command)
+    execute_command_detached(fod, command)
